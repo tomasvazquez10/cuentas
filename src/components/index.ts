f02c76a78a5228014b7b7c4b1d1961c55b7204ec
tomasvazquez10 <138628519+tomasvazquez10@users.noreply.github.com>
2026-07-24 15:24:25 -0300

    feat: Add components index

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+export { Button } from './Button';
+export { Input } from './Input';
+export { Card } from './Card';
+export { CustomModal } from './Modal';
+export { MovimientoCard } from './MovimientoCard';
+export { PagoCard } from './PagoCard';
+export { StatCard } from './StatCard';
